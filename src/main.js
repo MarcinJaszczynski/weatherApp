c6cfa749642de65144cd4ff799ebc49ac34818c9
Marcin Jaszczynski <m.jaszczynski@gmail.com>
2022-06-24 13:32:15 +0200

    Adding fading and switching views

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const getDOMElem = id => {
 }
 
 const connectHTMLElems = () => {
-	viewElems.mainContainer = getDOMElem('mainContainer') 
+	viewElems.mainContainer = getDOMElem('mainContainer')
 	viewElems.weatherSearchView = getDOMElem('weatherSearchView')
 	viewElems.weatherForecastView = getDOMElem('weatherForecastView')
 
@@ -27,6 +27,7 @@ const connectHTMLElems = () => {
 const setupListeners = () => {
 	viewElems.searchInput.addEventListener('keydown', onEnterSubmit)
 	viewElems.searchButton.addEventListener('click', onClickSubmit)
+	viewElems.returnToSearchBtn.addEventListener('click', returnToSearch)
 }
 
 const initializeApp = () => {
@@ -34,14 +35,56 @@ const initializeApp = () => {
 	setupListeners()
 }
 
-const onClickSubmit = () => {}
+const onClickSubmit = event => {
+	fadeInOut()
+	let query = viewElems.searchInput.value
+	getWeatherByCity(query).then(data => {
+		console.log(data)
+	})
+	setTimeout(() => {
+		switchView()
+		fadeInOut()
+	}, 500)
+}
+
 const onEnterSubmit = event => {
 	if (event.key === 'Enter') {
+		fadeInOut()
 		let query = viewElems.searchInput.value
-		getWeatherByCity(query).then(data=>{
-            console.log(data)
-        })
+		getWeatherByCity(query).then(data => {
+			console.log(data)
+		})
+		setTimeout(() => {
+			switchView()
+			fadeInOut()
+		}, 500)
 	}
+}
+
+const switchView = () => {
+	if (viewElems.weatherSearchView.style.display !== 'none') {
+		viewElems.weatherSearchView.style.display = 'none'
+		viewElems.weatherForecastView.style.display = 'flex'
+	} else {
+		viewElems.weatherSearchView.style.display = 'flex'
+		viewElems.weatherForecastView.style.display = 'none'
+	}
+}
+
+const fadeInOut = () => {
+	if (viewElems.mainContainer.style.opacity === '1' || viewElems.mainContainer.style.opacity === '') {
+		viewElems.mainContainer.style.opacity = '0'
+	} else {
+		viewElems.mainContainer.style.opacity = '1'
+	}
+}
+
+const returnToSearch = () => {
+	fadeInOut()
+	setTimeout(() => {
+		switchView()
+		fadeInOut()
+	}, 500)
 }
 
 document.addEventListener('DOMContentLoaded', initializeApp)
