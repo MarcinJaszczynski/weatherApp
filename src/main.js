60254a2085bf754b63112d56afbfc929a0558ff0
Marcin Jaszczynski <m.jaszczynski@gmail.com>
2022-06-24 12:28:00 +0200

    Added request chain handling

--- src/main.js
+++ src/main.js
@@ -1,40 +1,47 @@
 // Main JS file
-const viewElems = {};
+import { getWeatherByCity } from './apiService.js'
+
+const viewElems = {}
 
 const getDOMElem = id => {
 	return document.getElementById(id)
 }
 
 const connectHTMLElems = () => {
-    viewElems.mainContainer = getDOMElem('mainContainer')
+	viewElems.mainContainer = getDOMElem('mainContainer') 
 	viewElems.weatherSearchView = getDOMElem('weatherSearchView')
-    viewElems.weatherForecastView = getDOMElem('weatherForecastView')
+	viewElems.weatherForecastView = getDOMElem('weatherForecastView')
 
-    viewElems.searchInput = getDOMElem('searchInput')
-    viewElems.searchButton = getDOMElem('searchButton')
-    
-    viewElems.weatherCity = getDOMElem('weatherCity')
-    viewElems.weatherIcon = getDOMElem('weatherIcon')
-    viewElems.weatherCurrentTemp = getDOMElem('weatherCurrentTemp')
-    viewElems.weatherMaxTemp = getDOMElem('weatherMaxTemp')
-    viewElems.weatherMinTemp = getDOMElem('weatherMinTemp')
-    
-    viewElems.returnToSearchBtn = getDOMElem('returnToSearchBtn')
+	viewElems.searchInput = getDOMElem('searchInput')
+	viewElems.searchButton = getDOMElem('searchButton')
+
+	viewElems.weatherCity = getDOMElem('weatherCity')
+	viewElems.weatherIcon = getDOMElem('weatherIcon')
+	viewElems.weatherCurrentTemp = getDOMElem('weatherCurrentTemp')
+	viewElems.weatherMaxTemp = getDOMElem('weatherMaxTemp')
+	viewElems.weatherMinTemp = getDOMElem('weatherMinTemp')
+
+	viewElems.returnToSearchBtn = getDOMElem('returnToSearchBtn')
 }
 
 const setupListeners = () => {
-    viewElems.searchInput.addEventListener('keydown', onEnterSubmit)
-    viewElems.searchButton.addEventListener('click', onClickSubmit)
+	viewElems.searchInput.addEventListener('keydown', onEnterSubmit)
+	viewElems.searchButton.addEventListener('click', onClickSubmit)
 }
-
-
 
 const initializeApp = () => {
 	connectHTMLElems()
 	setupListeners()
 }
 
-const onClickSubmit = ()=> {}
-const onEnterSubmit = () => {}
+const onClickSubmit = () => {}
+const onEnterSubmit = event => {
+	if (event.key === 'Enter') {
+		let query = viewElems.searchInput.value
+		getWeatherByCity(query).then(data=>{
+            console.log(data)
+        })
+	}
+}
 
 document.addEventListener('DOMContentLoaded', initializeApp)
